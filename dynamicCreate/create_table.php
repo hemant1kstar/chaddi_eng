b3d3fec4520193858e27d103ccb6d6f7b622fb84
brushikesh <brushikesh@resoneuronance.com>
2015-08-13 18:49:21 +0530

    create_table_modified1

--- dynamicCreate/create_table.php
+++ dynamicCreate/create_table.php
@@ -1,20 +1,48 @@
-<?php
-         include("database/connection.php");
-?>
 <html>
     <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="mdl/material.min.css">
     <link rel="stylesheet" href="css/add.css">
     <script src="mdl/material.min.js"></script>
     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons"> 
     <title>Create Tables Page</title>
-    
-<!--        it must for checkbox select-->
-    <script src="http://ajax.googleapis.com/ajax/libs/jquery/2.1.0/jquery.min.js"></script>
+
     </head>
 <body>
+<?php
+         include("database/connection.php");
+?>
+    <div class="mdl-shadow--2dp">
+        <form action="createtable.php" method="post">
+        <div class="mdl-grid">
+          <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label mdl-cell mdl-cell--8-col-tablet mdl-cell--4-col">
+                    <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+                         <input class="mdl-textfield__input" type="text" id="table_name" name="table_name"/>
+                         <label class="mdl-textfield__label" for="table_name">Enter Table Name</label>
+                    </div>
+          </div>
+        <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label mdl-cell mdl-cell--8-col-tablet mdl-cell--4-col">
+           <button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent" id="btn_add_fields">
+            Add Fields
+           </button>
+        </div>
+      </div>
+        
+       <div class="mdl-grid" id="add_fields_div">
+ 
+
+        </div>
+        
+        <div class="mdl-grid">
+    <input class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent" type="submit" name="submit"/>
+        </div>
+        </form>
+  </div>
+<!--
     <form name="" method="post" action="createtable.php">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+
             <table class="mdl-data-table mdl-js-data-table  mdl-shadow--2dp dataadd" id="container">
                 <tr>
                 <th class='mdl-data-table__cell--non-numeric' colspan="2">Enter TAble Name</th>
@@ -46,5 +74,12 @@
                 </tr>
             </table>  
     </form>
+-->
+         <script src="http://ajax.googleapis.com/ajax/libs/jquery/2.1.0/jquery.min.js"></script>   
+    <script type="text/javascript">
+            $("#btn_add_fields").click(function () {
+              $("#add_fields_div").append('<div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label mdl-cell mdl-cell--8-col-tablet mdl-cell--6-col"><input class="mdl-textfield__input" type="text" id="column_name" name="check_list1[]"/><label class="mdl-textfield__label" for="column_name">Enter Filed Name</label></div><div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label mdl-cell mdl-cell--8-col-tablet mdl-cell--6-col"><select name="datatype[]"><option value="INTEGER">int</option><option value="VARCHAR(255)">VARCHAR</option><option value="date">DATE</option></select></div>');
+            });
+        </script>
 </body>
 </html>
