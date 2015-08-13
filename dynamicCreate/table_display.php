7bd37a21f26c38db572af578f663b1da37363028
brushikesh <brushikesh@resoneuronance.com>
2015-08-13 17:40:36 +0530

    dynamicCretable modified

--- dynamicCreate/table_display.php
+++ dynamicCreate/table_display.php
@@ -20,97 +20,72 @@
         include("database/connection.php");
         if (isset($_GET['q'])) {  //get value as table name from url
         $link=$_GET['q'];
-                 mysqli_query ($con,"set character_set_results='utf8'"); 
-        $result=mysqli_query($con,"SELECT * FROM $link");//select value from perticular table
-        $query="DESCRIBE $link"; //build query for get column names from table
-        $results=mysqli_query($con,$query) or die('Query error:'.mysql_error());
+        mysqli_query ($con,"set character_set_results='utf8'"); 
         }
 ?>  
     
     
     <div class="">
         
-      <form name="frmUser" method="post" action="edit_user.php?q=<?php echo $link; ?>">
+        
+<!--      <form name="frmUser" method="post" action="edit_user.php?q=<?php echo $link; ?>">-->
             <table class="mdl-data-table mdl-js-data-table  mdl-shadow--2dp">
-                <?php
-                    echo "<tr><th class='mdl-data-table__cell--non-numeric'></th>";
-                    $columnname=array();
-                    $count=0;
-               
-                    while($row1 = mysqli_fetch_array($results)) {
-                        $columnname[$count]=$row1[0];
-                        $columnname1=ucwords($columnname[$count]);
-                        echo "<th class='mdl-data-table__cell--non-numeric'>$columnname1</th>";//all column names in this array
-                        $count++;
-                    }
-                     echo"</tr>";
-                ?>
-                <?php
-                    $i=0;
-                    $arraylen=sizeof($columnname); //find array length
-                while($row = mysqli_fetch_array($result)) {  
-                ?>
-                <tr>
-                    <td><input type="checkbox" name="id[]" class="mdl-data-table--selectable" value="<?php echo $row["id"];?>">
-                    </td>
-                <?php
-                    for($co=0;$co<$arraylen;$co++){
-                        $name=$columnname[$co];       
-                ?> 
-                <td><?php echo $row[$name];?></td>
-                <?php
-                    $i++;
-                    }
-                ?>
-                 </tr>
-                <?php
-                    $co++;
-                }
-                ?>
-                
+                       <thead>
+                    <tr>
+                        <?php
+                             $query="DESCRIBE $link"; //build query for get column names from table
+                             $results=mysqli_query($con,$query) or die('Query error:'.mysql_error());
+
+                             $columnname=array();
+                             $count=0;
+                              while($row1= mysqli_fetch_array($results)) {
+                                $columnname[$count]=$row1[0];
+                                $columnname1=ucwords($row1[0]);
+                                echo "<th class='mdl-data-table__cell--non-numeric'>$columnname1</th>";
+                                $count++;
+                              }
+                          echo "<th class='mdl-data-table__cell--non-numeric'>Update</th>";
+                          echo "<th class='mdl-data-table__cell--non-numeric'>Delete</th>";
+
+                        ?>
+                    </tr>
+                  </thead>
+                  <tbody>
+                      <?php
+                            $arraylen=sizeof($columnname); //find array length
+                            $result=mysqli_query($con,"SELECT * FROM $link");//select value from perticular table
+                            while($row2= mysqli_fetch_array($result)) {
+                                  echo "<tr>";
+                            for($co=0;$co<$arraylen;$co++){  
+                                $col_value=$row2[$co];
+                                echo "<th class='mdl-data-table__cell--non-numeric'>$col_value</th>";
+                         
+                                 }
+                echo "<th><a class='mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent' href='edit_user.php?q=$link'>Update</a></th>";
+                                         echo "<th><a class='mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent' href='table_for_delete.php?q=$link'>Delete</a></th>";
+     
+
+                                   echo "<tr>";
+                                   $co++;
+                            }
+                      ?>
+                  </tbody>
                  </table>
             
+<!--
                          <input type="submit" name="update" value="Update"  id="checkBtn" class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent submitbtn"/>  
                     <button type='button' class='mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent abutton'><a href='addData.php?q=$link' >Add</a></button>
                        <button type='button' class='mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent abutton'><a href='table_for_delete.php?q=$link' id='checkBtn1' >Delete</a></button>
+-->
                   
            
-    </form>  
+<!--    </form>  -->
     
     </div>
     
     
     
-    
-    
-         <!--        update-->
-            <script type="text/javascript">
-                $(document).ready(function(){
-                    $('#checkBtn').click(function(){
-                      checked = $("input[type=checkbox]:checked").length;
-                           
-                      if(!checked) {
-                        alert("You must check at least one checkbox for update.");
-                        return false;
-                      }else{
-                      }
-                    });
-                });
-            </script>
-        <!--        delete-->
-            <script type="text/javascript">
-                $(document).ready(function(){
-                    $('#checkBtn1').click(function(){
-                      checked = $("input[type=checkbox]:checked").length;
-                           
-                      if(!checked) {
-                        alert("You must check at least one checkbox for Delete.");
-                        return false;
-                      }else{
-                      }
-                    });
-                });
-            </script>   
+
       
 </body>
 </html>  
