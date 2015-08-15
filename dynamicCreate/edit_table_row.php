1faf7d7c7b391de12716e1bd8ee5464741153ebc
brushikesh <brushikesh@resoneuronance.com>
2015-08-15 14:32:47 +0530

    modified

--- dynamicCreate/edit_table_row.php
+++ dynamicCreate/edit_table_row.php
@@ -15,66 +15,87 @@
     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons"> 
     </head>
 <body>
-    <div class='mdl-grid'>
-    <?php
-        if(isset($_POST["submit_edit"]))
-        {
-           $dCreate_id=$_GET['Create_id'];
-//            echo "$dCreate_id<br/>";
-
-//            $columnname1=array();
-//            $count1=1;
-              
-              foreach($_POST["col_name"] as $cna=>$value1){
-                      $row5= mysqli_fetch_array($results);
-                      $columnname1=$row5[0];
-                      echo "columnname: $columnname1<br/>";
-                            echo "columnvalue: $value1<br/>";
-                  if($columnname1=='id'){
-                  
-                  }else{
-                         mysqli_query($con,"UPDATE $link set $columnname1='$value1' WHERE id='$dCreate_id'"); 
-                  }
-               }
-//              $count1++;
-
-        }
-?>
-    </div>
     
-    <form method="post" action="">
-        <table class="mdl-data-table mdl-js-data-table mdl-data-table--selectable mdl-shadow--2dp dataadd">
-        <?php
+    <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
+  <header class="mdl-layout__header">
+    <div class="mdl-layout__header-row">
+      <!-- Title -->
+      <span class="mdl-layout-title">Dynamic Content</span>
+      <!-- Add spacer, to align navigation to the right -->
+      <div class="mdl-layout-spacer"></div>
+      <!-- Navigation. We hide it in small screens. -->
+      <nav class="mdl-navigation mdl-layout--large-screen-only">
+        <a class="mdl-navigation__link" href="./index.php">Home</a>
+      </nav>
+    </div>
+  </header>
+  <div class="mdl-layout__drawer">
+    <span class="mdl-layout-title">Title</span>
+    <nav class="mdl-navigation">
+      <a class="mdl-navigation__link" href="./index.php">Home</a>
+    </nav>
+  </div>
+  <main class="mdl-layout__content">
+    <div class="page-content">
+        <!-- Your content goes here -->
+    <div class='mdl-grid'>
+            <?php
+                if(isset($_POST["submit_edit"]))
+                {
+                   $dCreate_id=$_GET['Create_id'];
+                      foreach($_POST["col_name"] as $cna=>$value1){
+                              $row5= mysqli_fetch_array($results);
+                              $columnname1=$row5[0];
+                              echo "columnname: $columnname1<br/>";
+                                    echo "columnvalue: $value1<br/>";
+                          if($columnname1=='id'){
 
-            $columnname=array();
-            $count=0;
-                while($row1 = mysqli_fetch_array($results)) {
-                    $columnname[$count]=$row1[0];
-                    $count++;
+                          }else{
+                                 mysqli_query($con,"UPDATE $link set $columnname1='$value1' WHERE id='$dCreate_id'"); 
+                          }
+                       }
+
                 }
-   
         ?>
-            
-        <?php
-                  $dCreate_id=$_GET['Create_id'];
-                  $result=mysqli_query($con,"SELECT * FROM $link WHERE id='$dCreate_id'");
-         echo "<input name='col_name[]' value='$dCreate_id' type='text'><br/>";
-            while($row1=mysqli_fetch_array($result))
-            { 
-               $arraylen=sizeof($columnname);
-               for($co=1;$co<$arraylen;$co++){ 
-                     $col_value=$row1[$co];
-                      echo "<input type='text' name='col_name[]' value='$col_value'><br/>";
-                }
-                $co++;
-            }
-          
-          ?>
+    </div>
+
+            <form method="post" action="">
+                <table class="mdl-data-table mdl-js-data-table mdl-data-table--selectable mdl-shadow--2dp dataadd">
+                <?php
+
+                    $columnname=array();
+                    $count=0;
+                        while($row1 = mysqli_fetch_array($results)) {
+                            $columnname[$count]=$row1[0];
+                            $count++;
+                        }
+                ?>
+
+                <?php
+                          $dCreate_id=$_GET['Create_id'];
+                          $result=mysqli_query($con,"SELECT * FROM $link WHERE id='$dCreate_id'");
+                 echo "<input name='col_name[]' value='$dCreate_id' type='text'><br/>";
+                    while($row1=mysqli_fetch_array($result))
+                    { 
+                       $arraylen=sizeof($columnname);
+                       for($co=1;$co<$arraylen;$co++){ 
+                             $col_value=$row1[$co];
+                              echo "<input type='text' name='col_name[]' value='$col_value'><br/>";
+                        }
+                        $co++;
+                    }
+                  ?>
 
 
-    <br/>
-    <input type="submit" name="submit_edit" value="Submit" class="mdl-button mdl-js-button mdl-button--raised submitbtn">
+            <br/>
+            <input type="submit" name="submit_edit" value="Submit" class="mdl-button mdl-js-button mdl-button--raised submitbtn">
 
-    </form>
+            </form>
+
+        
+      </div>
+  </main>
+</div>
+
 </body>
 </html>
